453f9f8c3d6103188f690e66c726774ca10e404b
Somendrat22 <somendrat22@gmail.com>
2023-12-12 22:52:04 +0530

    all files added and restructured

--- src/main/java/com/covivaccination/distributionsystem/Covid/Vaccination/Distribution/Backend/models/Doctor.java
+++ src/main/java/com/covivaccination/distributionsystem/Covid/Vaccination/Distribution/Backend/models/Doctor.java
@@ -1,17 +1,11 @@
 package com.covivaccination.distributionsystem.Covid.Vaccination.Distribution.Backend.models;
 
 import jakarta.persistence.*;
-import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
 public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -23,4 +17,64 @@ public class Doctor {
     int patientCount;
     @OneToMany
     List<Patient> patients;
+
+    public Doctor() {
+    }
+
+    public Doctor(UUID id, String name, String docDegree, VaccinationCenter vaccinationCenter, int patientCount, List<Patient> patients) {
+        this.id = id;
+        this.name = name;
+        this.docDegree = docDegree;
+        this.vaccinationCenter = vaccinationCenter;
+        this.patientCount = patientCount;
+        this.patients = patients;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDocDegree() {
+        return docDegree;
+    }
+
+    public void setDocDegree(String docDegree) {
+        this.docDegree = docDegree;
+    }
+
+    public VaccinationCenter getVaccinationCenter() {
+        return vaccinationCenter;
+    }
+
+    public void setVaccinationCenter(VaccinationCenter vaccinationCenter) {
+        this.vaccinationCenter = vaccinationCenter;
+    }
+
+    public int getPatientCount() {
+        return patientCount;
+    }
+
+    public void setPatientCount(int patientCount) {
+        this.patientCount = patientCount;
+    }
+
+    public List<Patient> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(List<Patient> patients) {
+        this.patients = patients;
+    }
 }
